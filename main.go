0ccf48214d2c112c4885816f902552c5025b17ce
wenxiaofei <2282186474@qq.com>
2020-03-28 21:33:08 +0800

    feat:add health check

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ func main() {
 	handler := http2.NewServer(endpoint, services.DecodeRequest, services.EncodeResponsefunc)
 	router := mux.NewRouter()
 	router.Handle("/user/{uid:\\d+}",handler)
+
+	//添加心跳检测
+	router.HandleFunc("/health", func(rspWriter http.ResponseWriter, request *http.Request) {
+		logs.Debug("there is the heart check ....")
+		rspWriter.Header().Set("Content-type","application-json")
+		rspWriter.Write([]byte(`{"status":"ok"}`))
+	})
+
 	//router.Methods("GET","DELETE")
 	logs.Debug("the server is starting.....")
 	http.ListenAndServe(":2345", router)
