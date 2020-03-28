39baf7ebecf04eebc0d34a664387fdd6bdeb53e5
wenxiaofei <2282186474@qq.com>
2020-03-28 23:40:09 +0800

    feat:add deregister server and register server

---
+++ data.sh
@@ -0,0 +1,11 @@
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_ec7bfc09-b2c5-e12c-f134-cd6a3f70415b
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_dbe65d4c-5578-178d-0f9f-1a0490339155
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_884cd9a4-301b-2c72-d39f-abfcafe056c8
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_67b590cd-f2c3-151b-6c17-d1749fc5647d
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_6574437e-bd36-334a-f16e-e5cf62e3a153
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_3a875ac1-0408-e0af-7af0-90a0223b3677
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_33628b94-c7ed-2346-ed63-51f763e07210
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_001d877b-8a44-b628-1e66-af499be00bd4
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/myserver1
+curl -XPUT http://localhost:8500/v1/agent/service/deregister/userService_884cd9a4-301b-2c72-d39f-abfcafe056c8
+
