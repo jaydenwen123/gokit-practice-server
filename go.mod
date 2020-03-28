77d816b182c5bb5d9b00d02cae185b78df55235e
wenxiaofei <2282186474@qq.com>
2020-03-28 22:07:45 +0800

    feat:add consul register service

--- go.mod
+++ go.mod
@@ -3,10 +3,20 @@ module github.com/jaydenwen123/gokit-practice-server
 go 1.13
 
 require (
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
 	github.com/astaxie/beego v1.12.1
 	github.com/go-kit/kit v0.9.0
 	github.com/go-logfmt/logfmt v0.5.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gorilla/mux v1.7.4
-	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/hashicorp/consul/api v1.4.0
+	github.com/hashicorp/go-immutable-radix v1.1.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/hashicorp/memberlist v0.1.6 // indirect
+	github.com/hashicorp/serf v0.8.5 // indirect
+	github.com/jaydenwen123/go-util v0.0.0-20200320104025-03e082c3c450
+	github.com/miekg/dns v1.1.26 // indirect
+	github.com/stretchr/testify v1.4.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jaydenwen123/go-util"
 	"net/http"
 
 	"github.com/astaxie/beego/logs"
 	http2 "github.com/go-kit/kit/transport/http"
-	"github.com/jaydenwen123/gokit-practice-server/services"
 	"github.com/gorilla/mux"
+	"github.com/jaydenwen123/gokit-practice-server/services"
+
+	consulapi "github.com/hashicorp/consul/api"
 )
 
 func main() {
@@ -14,16 +18,106 @@ func main() {
 	endpoint := services.MakeUserEndpoint(userService)
 	handler := http2.NewServer(endpoint, services.DecodeRequest, services.EncodeResponsefunc)
 	router := mux.NewRouter()
-	router.Handle("/user/{uid:\\d+}",handler)
+	router.Handle("/user/{uid:\\d+}", handler)
 
 	//添加心跳检测
 	router.HandleFunc("/health", func(rspWriter http.ResponseWriter, request *http.Request) {
 		logs.Debug("there is the heart check ....")
-		rspWriter.Header().Set("Content-type","application-json")
+		rspWriter.Header().Set("Content-type", "application-json")
 		rspWriter.Write([]byte(`{"status":"ok"}`))
 	})
 
 	//router.Methods("GET","DELETE")
 	logs.Debug("the server is starting.....")
-	http.ListenAndServe(":2345", router)
+	RegisterService("myserver2","userService","127.0.0.1",2346)
+	http.ListenAndServe(":2346", router)
+}
+
+//服务注册
+func RegisterService( id, name,address string, port int,) {
+	client := makeClient()
+	agent := client.Agent()
+	reg := &consulapi.AgentServiceRegistration{
+		Name:    name,
+		ID:      id,
+		Tags:    []string{"primary", "backup"},
+		Address: address,
+		Port:    2346,
+		Check: &consulapi.AgentServiceCheck{
+			HTTP: fmt.Sprintf("http://%s:%d/health",address,port),
+			Interval:"10s",
+		},
+	}
+	if err := agent.ServiceRegister(reg); err != nil {
+		logs.Error("ServiceRegister err: %v", err)
+	}
+
+	services, err := agent.Services()
+	if err != nil {
+		logs.Error("err: %v", err)
+	}
+	logs.Debug("the services:%s", util.Obj2JsonStr(services))
+	//if _, ok := services["myserver2"]; !ok {
+	//logs.Error("missing service: %#v", services)
+	//}
+	checks, err := agent.Checks()
+
+	if err != nil {
+		logs.Error("get checks err: %v", err)
+	}
+	logs.Debug("the checks:%s", util.Obj2JsonStr(checks))
+	//chk, ok := checks["service:foo"]
+	//if !ok {
+	//	logs.Error("missing check: %v", checks)
+	//}
+
+	// Checks should default to critical
+	//if chk.Status != consulapi.HealthCritical {
+	//	logs.Error("Bad: %#v", chk)
+	//}
+
+	//state, out, err := agent.AgentHealthServiceByID("foo2")
+	//require.Nil(t, err)
+	//require.Nil(t, out)
+	//require.Equal(t, HealthCritical, state)
+	//
+	//state, out, err = agent.AgentHealthServiceByID("foo")
+	//require.Nil(t, err)
+	//require.NotNil(t, out)
+	//require.Equal(t, HealthCritical, state)
+	//require.Equal(t, 8000, out.Service.Port)
+	//
+	//state, outs, err := agent.AgentHealthServiceByName("foo")
+	//require.Nil(t, err)
+	//require.NotNil(t, outs)
+	//require.Equal(t, HealthCritical, state)
+	//require.Equal(t, 8000, outs[0].Service.Port)
+
+	if err = agent.ServiceDeregister("foo"); err != nil {
+		logs.Error("the ServiceDeregister failed  err: %v", err)
+	} else {
+		logs.Debug("the ServiceDeregister success...")
+	}
+}
+
+func makeClient() *consulapi.Client {
+
+	return makeClientWithConfig(nil)
+}
+
+type configCallback func(c *consulapi.Config)
+
+func makeClientWithConfig(cb1 configCallback) *consulapi.Client {
+	// Make client config
+	conf := consulapi.DefaultConfig()
+	if cb1 != nil {
+		cb1(conf)
+	}
+
+	// Create client
+	client, err := consulapi.NewClient(conf)
+	if err != nil {
+		logs.Error("new consul Client  err: %v", err)
+	}
+	return client
 }
